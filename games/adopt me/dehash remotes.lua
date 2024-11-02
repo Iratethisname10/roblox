1fc0c5905ed9468424c2709be201fd43725c08e1
IRateThisName10 <115908538+Iratethisname10@users.noreply.github.com>
2024-11-02 08:32:48 +0800

    Update dehash remotes.lua

--- games/adopt me/dehash remotes.lua
+++ games/adopt me/dehash remotes.lua
@@ -3,7 +3,7 @@ local router;
 for _, v in next, getgc(true) do
 	if (type(v) ~= 'table') then continue; end;
 
-	if rawget(v, 'get_remote_from_cache') then
+	if (rawget(v, 'get_remote_from_cache')) then
 		router = v;
 	end;
 end;
