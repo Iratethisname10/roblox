d30c6750f27dbc541de23c47bd9596d4c50ead5a
IRateThisName10 <115908538+Iratethisname10@users.noreply.github.com>
2024-11-02 08:32:02 +0800

    Add files via upload

---
+++ games/adopt me/dehash remotes.lua
@@ -0,0 +1,15 @@
+local router;
+
+for _, v in next, getgc(true) do
+	if (type(v) ~= 'table') then continue; end;
+
+	if rawget(v, 'get_remote_from_cache') then
+		router = v;
+	end;
+end;
+
+local remotes = debug.getupvalue(router.get_remote_from_cache, 1);
+
+for k, v in next, remotes do
+	v.Name = k;
+end;
